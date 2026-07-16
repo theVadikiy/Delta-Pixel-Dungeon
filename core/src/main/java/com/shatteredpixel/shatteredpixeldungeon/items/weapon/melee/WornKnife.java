d9793cc5cccb70fbceb6675281a5dbf5ef21a44b
theVadikiy <vadya456789@gmail.com>
2026-07-16 19:29:32 +0400

    Update WornKnife.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/WornKnife.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/WornKnife.java
@@ -76,3 +76,22 @@ public class WornKnife extends MeleeWeapon {
 	}
 
 }
+
+	public MagesStaff(Wand wand){
+		this();
+		wand.identify();
+		wand.cursed = false;
+		this.wand = wand;
+		updateWand(false);
+		wand.curCharges = wand.maxCharges;
+	}
+
+	@Override
+	public ArrayList<String> actions(Hero hero) {
+		ArrayList<String> actions = super.actions( hero );
+		actions.add(AC_IMBUE);
+		if (wand!= null && wand.curCharges > 0) {
+			actions.add( AC_ZAP );
+		}
+		return actions;
+	}
