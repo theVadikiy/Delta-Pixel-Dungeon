3aba02086cca20296b7e9e57c73b2b554508c503
Vadikiy <vadya456789@gmail.com>
2026-07-21 22:33:39 +0400

    forgot to import

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/WornKnife.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/WornKnife.java
@@ -34,6 +34,7 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.Char;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
 import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.watabou.noosa.audio.Sample;
 
 import java.util.ArrayList;
 
