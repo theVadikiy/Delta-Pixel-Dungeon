1cf00c9963dc57470906cd5214a05ffb14b5a2c9
Vadikiy <vadya456789@gmail.com>
2026-07-22 12:42:50 +0400

    added text to fountain creation

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/WornKnife.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/weapon/melee/WornKnife.java
@@ -99,7 +99,7 @@ public ArrayList<String> actions(Hero hero) {
 
 @Override
 public void execute(Hero hero, String action) {
-    if (action.equals(AC_USE) || action.equals(AC_FOUNTAIN)) { 
+    if (action.equals(AC_FOUNTAIN)) { 
 		hero.sprite.play(hero.sprite.ATTACK);
 		wait(0.5f);
 		Sample.INSTANCE.play( Assets.Sounds.DARK_FOUNTAIN);
